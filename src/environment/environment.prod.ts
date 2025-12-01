1db70b727fbf28ef8fb0c1f1ffc1f5451a541416
Hasan Uddin <abcnizamd@gmail.com>
2025-12-01 15:35:20 +0600

    ✨ env updated

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -3,6 +3,6 @@ import { EnvironmentConfig } from '../libs/core/environment-config.model'
 export const environment: EnvironmentConfig = {
     appName: 'Auth-server',
     production: true,
-    apiUrl: 'https://jsonplaceholder.typicode.com',
-    authApiUrl: '',
+    apiUrl: 'https://authapi.dapplesoft.com',
+    authApiUrl: 'https://authapi.dapplesoft.com',
 }
--- src/environment/environment.staging.ts
+++ src/environment/environment.staging.ts
@@ -3,6 +3,6 @@ import { EnvironmentConfig } from '../libs/core/environment-config.model'
 export const environment: EnvironmentConfig = {
     appName: 'Auth-serverBeta',
     production: true,
-    apiUrl: 'https://jsonplaceholder.typicode.com',
-    authApiUrl: '',
+    apiUrl: 'https://authapi.dapplesoft.com/api',
+    authApiUrl: 'https://authapi.dapplesoft.com/api',
 }
