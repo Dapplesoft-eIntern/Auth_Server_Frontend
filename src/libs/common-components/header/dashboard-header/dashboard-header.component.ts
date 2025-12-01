b8762018604e66b2d4fb6442450b9cc5a12d4528
Hasan Uddin <abcnizamd@gmail.com>
2025-12-01 21:32:21 +0600

    logout functionality ✨

--- src/libs/common-components/header/dashboard-header/dashboard-header.component.ts
+++ src/libs/common-components/header/dashboard-header/dashboard-header.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core'
-import { RouterModule } from '@angular/router'
+import { Component, inject } from '@angular/core'
+import { Router, RouterModule } from '@angular/router'
+import { AuthService } from '../../../auth/service/auth.service'
 @Component({
     selector: 'app-dashboard-header',
     imports: [RouterModule],
     templateUrl: './dashboard-header.component.html',
     styleUrl: './dashboard-header.component.css',
 })
-export class DashboardHeaderComponent {}
+export class DashboardHeaderComponent {
+    private router = inject(Router)
+    constructor(private authserver: AuthService) {}
+    logOut() {
+        this.authserver.logout()
+        this.router.navigate(['/login'])
+    }
+}
