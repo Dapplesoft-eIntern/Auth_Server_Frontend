5d0676e6a67a828fa849f05f3ad2befde945f486
Hasan Uddin <abcnizamd@gmail.com>
2025-12-04 21:06:05 +0600

    jwt interception ✅

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,15 @@ import { bootstrapApplication } from '@angular/platform-browser'
 import { AppComponent } from './app/app.component'
 import { appConfig } from './app/app.config'
 import 'flowbite'
+import { provideHttpClient, withInterceptors } from '@angular/common/http'
+import { AuthHeaderInterceptorFn } from './libs/auth'
 
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err))
+bootstrapApplication(AppComponent, {
+    ...appConfig, // keep existing config
+    providers: [
+        ...(appConfig.providers || []), // keep other providers
+        provideHttpClient(
+            withInterceptors([AuthHeaderInterceptorFn]), // interceptor
+        ),
+    ],
+}).catch((err) => console.error(err))
