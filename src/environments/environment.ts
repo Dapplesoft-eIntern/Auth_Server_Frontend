c66f25179a3edf876f83fdcd85161833cd09d4aa
Hasan Uddin <abcnizamd@gmail.com>
2025-12-06 21:43:19 +0600

    Env fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,3 +7,11 @@ export const environment: EnvironmentConfig = {
     apiUrl: 'https://authapi.dapplesoft.com/api',
     authApiUrl: 'https://authapi.dapplesoft.com/api',
 }
+
+// export const environment: EnvironmentConfig = {
+//     BaseUrl: 'http://localhost:4200',
+//     appName: 'Auth-server',
+//     production: false,
+//     apiUrl: 'http://localhost:5000/api',
+//     authApiUrl: 'http://localhost:5000/api',
+// }
