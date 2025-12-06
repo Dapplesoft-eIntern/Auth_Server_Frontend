48970db8776ee2ad19f783bfca7f4ba94ea9054f
Hasan Uddin <abcnizamd@gmail.com>
2025-12-06 23:08:23 +0600

    edit user modal

---
+++ src/libs/user/components/edit-user-modal/edit-user-modal.component.ts
@@ -0,0 +1,73 @@
+import { Component, inject, signal } from '@angular/core'
+import { ReactiveFormsModule } from '@angular/forms'
+import { ButtonModule } from 'primeng/button'
+import { DialogModule } from 'primeng/dialog'
+import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog'
+import { FloatLabelModule } from 'primeng/floatlabel'
+import { InputTextModule } from 'primeng/inputtext'
+import { AlertService } from '../../../common-service/lib/alert.service'
+import { UserFormService } from '../../user.form.service'
+import { User } from '../../user.model'
+import { UserApiService } from '../../user-api.service'
+
+@Component({
+    selector: 'app-edit-user-modal',
+    imports: [
+        ButtonModule,
+        DialogModule,
+        ReactiveFormsModule,
+        FloatLabelModule,
+        InputTextModule,
+    ],
+    templateUrl: './edit-user-modal.component.html',
+    styleUrl: './edit-user-modal.component.css',
+})
+export class EditUserModalComponent {
+    public userFormService = inject(UserFormService)
+    private userApiService = inject(UserApiService)
+    private alertService = inject(AlertService)
+    private ref = inject(DynamicDialogRef)
+    private config = inject(DynamicDialogConfig)
+
+    isLoading = signal<boolean>(false)
+    isError = signal<boolean>(false)
+
+    ngOnInit() {
+        const selectedUser: User = this.config.data?.user
+        if (selectedUser) {
+            this.userFormService.patchForm(selectedUser)
+        }
+    }
+
+    submit(event: Event) {
+        this.isLoading.set(true)
+        event.preventDefault()
+
+        const selectedUser = this.config.data?.user
+        const formValue = this.userFormService.getValue()
+
+        const userData: Partial<User> = {
+            ...formValue,
+            id: selectedUser.id,
+        }
+
+        this.updateUser(userData)
+    }
+
+    updateUser(userData: Partial<User>) {
+        this.userApiService.updateUser(userData).subscribe({
+            next: (res) => {
+                this.userFormService.form.reset()
+                this.isLoading.set(false)
+                this.alertService.success('User updated successfully')
+                this.ref.close(userData)
+            },
+            error: () => {
+                this.isLoading.set(false)
+                this.isError.set(true)
+                this.alertService.error('Failed to update user')
+                this.ref.close()
+            },
+        })
+    }
+}
