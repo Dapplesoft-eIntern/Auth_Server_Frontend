964d5381b3cd871b35f5a08e975317fa40e115e1
Hasan Uddin <abcnizamd@gmail.com>
2025-12-06 13:53:21 +0600

    🐛 fixed

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,5 @@ import { bootstrapApplication } from '@angular/platform-browser'
 import { AppComponent } from './app/app.component'
 import { appConfig } from './app/app.config'
 import 'flowbite'
-import { provideHttpClient, withInterceptors } from '@angular/common/http'
-import { AuthHeaderInterceptorFn } from './libs/auth'
 
-bootstrapApplication(AppComponent, {
-    ...appConfig, // keep existing config
-    providers: [
-        ...(appConfig.providers || []), // keep other providers
-        provideHttpClient(
-            withInterceptors([AuthHeaderInterceptorFn]), // interceptor
-        ),
-    ],
-}).catch((err) => console.error(err))
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err))
