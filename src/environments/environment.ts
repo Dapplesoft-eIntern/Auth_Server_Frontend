7b293f1a6c11dbea368d00a4be8155450c83841c
laptop <masumahmed1438@gmail.com>
2025-12-07 19:55:19 +0600

    update auditlog and role

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,17 @@
 import { EnvironmentConfig } from '../libs/core/environment-config.model'
 
-// export const environment: EnvironmentConfig = {
-//     BaseUrl: 'http://localhost:4200',
-//     appName: 'Auth-server',
-//     production: false,
-//     apiUrl: 'https://authapi.dapplesoft.com/api',
-//     authApiUrl: 'https://authapi.dapplesoft.com/api',
-// }
-
 export const environment: EnvironmentConfig = {
     BaseUrl: 'http://localhost:4200',
     appName: 'Auth-server',
     production: false,
-    apiUrl: 'https://localhost:5001/api',
-    authApiUrl: 'https://localhost:5001/api',
+    apiUrl: 'https://authapi.dapplesoft.com/api',
+    authApiUrl: 'https://authapi.dapplesoft.com/api',
 }
+
+// export const environment: EnvironmentConfig = {
+//     BaseUrl: 'http://localhost:4200',
+//     appName: 'Auth-server',
+//     production: false,
+//     apiUrl: 'https://localhost:5001/api',
+//     authApiUrl: 'https://localhost:5001/api',
+// }
